e937a38aba7536155523bc626c8ad4afb8d436ac
LordRonz <aarontanhar2000@gmail.com>
2021-12-07 16:24:25 +0700

    feat: removes 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,11 @@ import './styles/globals.css';
 
 import React, { lazy, Suspense } from 'react';
 import { Helmet } from 'react-helmet';
-import { Route, Routes } from 'react-router';
 
 import Home from './components/Home';
 
 const About = lazy(() => import('./components/About'));
 const Footer = lazy(() => import('./components/Footer'));
-const NotFoundPage = lazy(() => import('./components/404'));
 
 export const Main = (): JSX.Element => (
   <div className="App bg-gray-900 body-font">
@@ -25,24 +23,7 @@ export const Main = (): JSX.Element => (
 );
 
 const App = (): JSX.Element => (
-  <Routes>
-    <Route
-      path="/"
-      element={(
-        <Suspense fallback={<div>Loading...</div>}>
-          <Main />
-        </Suspense>
-    )}
-    />
-    <Route
-      path="*"
-      element={(
-        <Suspense fallback={<div>Loading...</div>}>
-          <NotFoundPage />
-        </Suspense>
-    )}
-    />
-  </Routes>
+  <Main />
 );
 
 export default App;
